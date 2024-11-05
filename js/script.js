fec0c70ca2aa975ea62cbcc89be64e5ed49035de
cadenh10 <cadenleeho@gmail.com>
2024-11-05 15:53:28 -0600

    updated script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 function init(){
-//add your javascrip between these two lines of code
+  const name = "Caden Ho";
+  
+  const alertMeDiv = document.getElementById("alertMe");
+  const textInput = document.getElementById("textInput");
+  const displayText = document.getElementById("displayText");
+  
+  alertMeDiv.addEventListener("click, function() {
+	  const inputValue = textInput.value;
+	  alert(`${name}: ${inputValue}`);
+	  displayText.textContent = inputValue;
+  });
+}
+  
  
-
-
-
-
-
-
-
 window.addEventListener('load', init);
